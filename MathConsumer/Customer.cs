a781a291beb7d2e855ccbadf010594f3ab46433a
iskyldrm <isakyildirim@outlook.com>
2022-06-01 14:04:03 +0300

    Add project files.

---
+++ MathConsumer/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace MathConsumer
+{
+    public class Customer
+    {
+        public string CustomerId { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
---
+++ MathConsumer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text.Json;
+
+namespace MathConsumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("CustomerId");
+            var customerId = System.Console.ReadLine();
+
+            HttpWebRequest httpWebRequest = (HttpWebRequest)HttpWebRequest.Create("https://localhost:44379/math?CustomerId=" + customerId);
+            httpWebRequest.Method = "GET";
+
+            string test = string.Empty;
+
+            using (HttpWebResponse response = (HttpWebResponse)httpWebRequest.GetResponse())
+            {
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+                test = reader.ReadToEnd();
+                stream.Close();
+            }
+
+            var CustomerIdList = JsonSerializer.Deserialize<List<Customer>>(test, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            foreach (Customer item in CustomerIdList)
+            {
+                System.Console.WriteLine(item.CompanyName);
+            }
+        }
+    }
+}
